9db6fc14026541a1a6d45f8cb7bbc83b190304df
sfluhrer <sfluhrer@cisco.com>
2024-05-21 11:07:08 -0400

    Initial program

---
+++ gamma.h
@@ -0,0 +1,3 @@
+double compute_sec_level( double m, int H, int T, int K );
+int check_sec_level( double m, int H, int T, int K, double sec_level );
+int compute_sigs_at_sec_level( double sec_level, int H, int T, int K );
